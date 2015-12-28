e23404a7976f123b616bcf0c8401f881252d5a0f
Tops <tops@Topss-iMac.local>
2015-12-28 16:56:52 +0530

    Initial Commit

---
+++ LocalNotificationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNotificationApp
+//
+//  Created by Tops on 12/28/15.
+//  Copyright (c) 2015 Tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalNotificationApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalNotificationApp
+//
+//  Created by Tops on 12/28/15.
+//  Copyright (c) 2015 Tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
